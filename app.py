eaf2959fcab45f3a75006a308a5c93dd658ae271
David MP <davidmp2015@gmail.com>
2024-09-28 23:33:25 -0300

    Pagina novo_livro atualizado.1

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
 from flask import render_template, redirect, url_for, request, session, flash
+from flask import Flask, jsonify
 from config import app, db
-from models import Cliente
-from flask import jsonify
+from models import Cliente, Livro  # Certifique-se de que Livro está importado corretamente
 from datetime import datetime
+import os
+from werkzeug.utils import secure_filename
+from config import app, db
+from models import Livro
 
 # Simulação de dados de login (usuário e senha)
 USERS = {
@@ -19,7 +23,7 @@ def home():
 # FIM HOME
 #####################################
 
-# LOGUIN
+# LOGIN
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
@@ -38,13 +42,12 @@ def logout():
     session.pop('username', None)
     return redirect(url_for('login'))
 
-#FIM LOGUIN
+# FIM LOGIN
 #########################################
 
-#CLIENTES
+# CLIENTES
 @app.route('/clientes', methods=['GET', 'POST'])
 def clientes():
-    # Verifica se o método é POST (tentativa de cadastro)
     if request.method == 'POST':
         nome_completo = request.form['nome_completo']
         data_nascimento = request.form['data_nascimento']
@@ -56,7 +59,6 @@ def clientes():
         profissao = request.form['profissao']
         escolaridade = request.form['escolaridade']
 
-        # Cria um novo objeto cliente
         novo_cliente = Cliente(
             nome_completo=nome_completo,
             data_nascimento=data_nascimento,
@@ -70,34 +72,27 @@ def clientes():
         )
 
         try:
-            db.session.add(novo_cliente)  # Adiciona o cliente ao banco
-            db.session.commit()  # Confirma a transação
+            db.session.add(novo_cliente)
+            db.session.commit()
             flash('Cliente cadastrado com sucesso!')
-            return redirect(url_for('clientes'))  # Redireciona para a mesma rota após o cadastro
+            return redirect(url_for('clientes'))
         except Exception as e:
-            db.session.rollback()  # Reverte em caso de erro
+            db.session.rollback()
             flash(f'Erro ao cadastrar cliente: {str(e)}')
             return redirect(url_for('clientes'))
-
-    # Lógica de filtro (GET request) para buscar clientes pelo nome
     else:
-        # Para o método GET, verifica se há um parâmetro de pesquisa
         nome = request.args.get('nome', '')
         if nome:
             clientes = Cliente.query.filter(Cliente.nome_completo.ilike(f'%{nome}%')).all()
         else:
             clientes = Cliente.query.all()
-        
+
     return render_template('clientes.html', clientes=clientes, filter_name=nome)
 
-
-# Rota para adicionar um novo cliente
 @app.route('/novo_cliente')
 def novo_cliente():
     return render_template('novo_cliente.html')
 
-
-# Rota para editar um cliente
 @app.route('/editar_cliente/<int:id>', methods=['GET', 'POST'])
 def editar_cliente(id):
     cliente = Cliente.query.get_or_404(id)
@@ -124,7 +119,6 @@ def editar_cliente(id):
 
     return render_template('editar_cliente.html', cliente=cliente)
 
-# Rota para excluir um cliente
 @app.route('/deletar_cliente/<int:id>', methods=['POST'])
 def deletar_cliente(id):
     cliente = Cliente.query.get_or_404(id)
@@ -137,10 +131,9 @@ def deletar_cliente(id):
         flash(f'Erro ao excluir cliente: {str(e)}')
     return redirect(url_for('clientes'))
 
-# Rota para pesquisar clientes
 @app.route('/cliente/<int:id>')
 def visualizar_cliente(id):
-    cliente = Cliente.query.get_or_404(id)  # Busca o cliente pelo ID ou retorna 404 se não encontrar
+    cliente = Cliente.query.get_or_404(id)
     return render_template('detalhes_cliente.html', cliente=cliente)
 
 @app.route('/cliente/<int:id>', methods=['GET'])
@@ -150,7 +143,7 @@ def detalhes_cliente(id):
     cliente_data = {
         'id': cliente.id,
         'nome_completo': cliente.nome_completo,
-        'data_nascimento': cliente.data_nascimento.strftime('%d/%m/%Y'),  # Formatar a data
+        'data_nascimento': cliente.data_nascimento.strftime('%d/%m/%Y'),
         'identidade': cliente.identidade,
         'telefone': cliente.telefone,
         'rua': cliente.rua,
@@ -162,17 +155,21 @@ def detalhes_cliente(id):
 
     return jsonify(cliente_data)
 
-#FIM CLIENTES
+# FIM CLIENTES
 #########################################
 
-#LIVROS
+# LIVROS
 @app.route('/livros')
 def livros():
     if 'username' not in session:
         return redirect(url_for('login'))
     return render_template('livros.html')
 
-#Rota para novo livro
+# Define o diretório para salvar as capas dos livros
+UPLOAD_FOLDER = 'static/uploads'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+# Rota para novo livro
 @app.route('/novo_livro', methods=['GET', 'POST'])
 def novo_livro():
     if request.method == 'POST':
@@ -180,11 +177,34 @@ def novo_livro():
         titulo = request.form['titulo']
         autor = request.form['autor']
         editora = request.form['editora']
-        ano = request.form['ano']
+        idade_leitura = request.form.get('idade_leitura')
         isbn = request.form['isbn']
+        ano = request.form['ano']
+        num_paginas = request.form['num_paginas']
+        valor = request.form['valor']
 
-        # Código para salvar o livro no banco de dados
-        novo_livro = Livro(titulo=titulo, autor=autor, editora=editora, ano=ano, isbn=isbn)
+        # Verifica se há uma capa de livro enviada
+        capa_livro = request.files['capa_livro']
+        capa_livro_filename = None
+        if capa_livro:
+            # Salva a capa com um nome seguro
+            capa_livro_filename = secure_filename(capa_livro.filename)
+            capa_livro.save(os.path.join(app.config['UPLOAD_FOLDER'], capa_livro_filename))
+
+        # Cria um novo objeto livro com os dados do formulário
+        novo_livro = Livro(
+            titulo=titulo,
+            autor=autor,
+            editora=editora,
+            idade_leitura=idade_leitura,
+            isbn=isbn,
+            ano=ano,
+            num_paginas=num_paginas,
+            valor=valor,
+            capa_livro=capa_livro_filename  # Adiciona a capa ao livro
+        )
+
+        # Adiciona e comita no banco de dados
         db.session.add(novo_livro)
         db.session.commit()
 
@@ -192,43 +212,20 @@ def novo_livro():
 
     return render_template('novo_livro.html')
 
-# Rota para criar novo livro
-@app.route('/novo_livro', methods=['GET', 'POST'])
-def criar_livro():
-    if request.method == 'POST':
-        # Pegar os dados do formulário
-        titulo = request.form['titulo']
-        autor = request.form['autor']
-        ano = request.form['ano']
-        isbn = request.form['isbn']
-
-        # Código para salvar o livro no banco de dados
-        novo_livro = Livro(titulo=titulo, autor=autor, ano=ano, isbn=isbn)
-        db.session.add(novo_livro)
-        db.session.commit()
-        data = request.get_json()
-        return jsonify(success=True), 201
-    return redirect(url_for('livros'))
-    print(request.form)
-
-    return render_template('novo_livro.html')
-
-
-#FIM LIVROS
+# FIM LIVROS
 #########################################
-#CONFIGURAÇAO
 
+# CONFIGURAÇÃO
 @app.route('/configuracao')
 def configuracao():
     if 'username' not in session:
         return redirect(url_for('login'))
     return render_template('configuracao.html')
 
-#FIM CONFIGURAÇAO
+# FIM CONFIGURAÇÃO
 #########################################
-
 
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()  # Certifique-se de que as tabelas estão sendo criadas
-    app.run(debug=True)    
+    app.run(debug=True)
--- models.py
+++ models.py
@@ -14,3 +14,26 @@ class Cliente(db.Model):
     escolaridade = db.Column(db.String(50), nullable=False)
     data_cadastro = db.Column(db.DateTime, default=datetime.utcnow)
 
+
+class Livro(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    titulo = db.Column(db.String(100), nullable=False)
+    autor = db.Column(db.String(100), nullable=False)
+    editora = db.Column(db.String(100), nullable=False)
+    idade_leitura = db.Column(db.String(50), nullable=True)
+    isbn = db.Column(db.String(20), nullable=False)
+    ano = db.Column(db.Integer, nullable=False)
+    num_paginas = db.Column(db.Integer, nullable=False)
+    valor = db.Column(db.Float, nullable=False)
+    capa_livro = db.Column(db.String(200), nullable=True)  # Novo campo para a capa
+
+    def __init__(self, titulo, autor, editora, idade_leitura, isbn, ano, num_paginas, valor, capa_livro):
+        self.titulo = titulo
+        self.autor = autor
+        self.editora = editora
+        self.idade_leitura = idade_leitura
+        self.isbn = isbn
+        self.ano = ano
+        self.num_paginas = num_paginas
+        self.valor = valor
+        self.capa_livro = capa_livro
